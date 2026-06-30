533058719fdfc02798278f92ffde62223d3ee842
Aluno Vocacao Eric Renan <ericrenansoaress@gmail.com>
2026-06-30 17:07:38 -0300

    Add files via upload

---
+++ listas_mod04_Ericrenan.py/dicionaro_mod04_ericrenan.py
@@ -0,0 +1,17 @@
+
+dias_semana = ('segunda-feira,terça-feira,quarta-feira,sexta-feira,sábado,domingo')
+cardapio_semana = {" "
+"sesegunda-feira": "Arroz feijao e bife",
+"terça-feira": "frango frito",
+"quarta-feira": "peixe",
+"quinta-feira": "lasanha",
+"sexta-feira": "feijoada",
+"sábado": "churrasco",
+"domingo": "pizza"
+}
+
+print('modulo04 Estruturas de dados Dicionários \n')
+print('-'*40)
+
+nome_usuario = input("Digite seu nome para começar:")
+dia_esolhido =input('Quede dia e hoje')
